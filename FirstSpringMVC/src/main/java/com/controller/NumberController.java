cfb0bbebd6884b5658b0ad46cc655416fc3d1dd7
Anusarani <anusarani@gmail.com>
2019-06-18 23:48:09 +0530

    first commit

---
+++ FirstSpringMVC/src/main/java/com/controller/NumberController.java
@@ -0,0 +1,33 @@
+package com.controller;
+
+import javax.servlet.http.*;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class NumberController implements InitializingBean{
+
+	
+	@RequestMapping("/addNumbers")
+	public ModelAndView addNumber(HttpServletRequest request,HttpServletResponse response)
+	{
+		int num1=Integer.parseInt(request.getParameter("number1"));
+		int num2=Integer.parseInt(request.getParameter("number2"));
+		int result = num1 + num2;
+		System.out.println("Result is not calculated");
+		ModelAndView modelAndView = new ModelAndView("Result.jsp", "Result", result);
+		modelAndView.addObject("TestObject", "Hello");
+		return modelAndView;
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Initialized Controller");
+		
+	}
+
+}
---
+++ FirstSpringMVC/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form action="addNumbers">
+
+	Number 1 <input type="text" name="number1"/><br>
+	Number 2 <input type="text" name="number2"/><br>
+	<input type="submit" name="ADD"/>
+</form>
+</body>
+</html>
